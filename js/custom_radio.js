d50a507c1875ca3b59168b3c42f08ce65faa0c39
cisco <cisco>
2016-05-10 12:01:43 +0200

    nouvelle fonction

--- js/custom_radio.js
+++ js/custom_radio.js
@@ -7,8 +7,9 @@ $(function (){
     $('.off').click(function(){
 
         //on déselectionne le précédent
-        $('.on').prev().prop('checked','false');
-        $('.on').removeClass('on').addClass('off');
+        //on précise que la selection se fait uniquement dans le "block" dans lequel on se trouve
+        $(this).parent().find('.on').prev().prop('checked','false');
+        $(this).parent().find('.on').removeClass('on').addClass('off');
 
         //on selection l'elt en cours
         $(this).removeClass('off').addClass('on');
@@ -20,6 +21,7 @@ $(function (){
     //vérif saisir
     $('#btn').click(function(){
         console.log($('input[name=choix]:checked').val());
+        console.log($('input[name=sexe]:checked').val());
     });
 
 });
