6bc8c2cc40ab922dc43c5ed3136118829c258e9f
cisco <cisco>
2016-05-10 11:56:20 +0200

    finalisation widget

--- js/custom_radio.js
+++ js/custom_radio.js
@@ -1,4 +1,25 @@
 $(function (){
 
-    
+    //on ajoute un span après avoir fait dispaitre le bouton initial
+    $('.custom_radio input[type=radio]').hide().after('<span class="off"></span>');
+
+    //evt de click sur les spans
+    $('.off').click(function(){
+
+        //on déselectionne le précédent
+        $('.on').prev().prop('checked','false');
+        $('.on').removeClass('on').addClass('off');
+
+        //on selection l'elt en cours
+        $(this).removeClass('off').addClass('on');
+        $(this).prev().prop('checked','true');
+
+    });
+
+
+    //vérif saisir
+    $('#btn').click(function(){
+        console.log($('input[name=choix]:checked').val());
+    });
+
 });
